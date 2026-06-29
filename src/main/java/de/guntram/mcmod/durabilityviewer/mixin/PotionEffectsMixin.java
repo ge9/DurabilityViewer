bde2d83bb6a13fd9bfec6da5d1dcf89ec33fc3a9
ge9 <ge9@users.noreply.github.com>
2026-06-29 14:09:41 +0900

    update 26.2

--- src/main/java/de/guntram/mcmod/durabilityviewer/mixin/PotionEffectsMixin.java
+++ src/main/java/de/guntram/mcmod/durabilityviewer/mixin/PotionEffectsMixin.java
@@ -1,19 +1,25 @@
 package de.guntram.mcmod.durabilityviewer.mixin;
 
 import de.guntram.mcmod.durabilityviewer.client.gui.GuiItemDurability;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphicsExtractor;
-import net.minecraft.client.gui.Gui;
+import net.minecraft.client.gui.Hud;
 import net.minecraft.client.DeltaTracker;
 import org.objectweb.asm.Opcodes;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Gui.class)
+@Mixin(Hud.class)
 public abstract class PotionEffectsMixin {
 
+    @Shadow
+    @Final
+    private Minecraft minecraft;
     @Unique
     private static GuiItemDurability gui;
 
